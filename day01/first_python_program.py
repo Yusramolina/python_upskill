ef0c73e2a6f3326c448a62811cdc948a0e274c92
Yusra <yusra.molina@gmail.com>
2025-04-10 17:42:18 +0200

    Python Programming

---
+++ day01/first_python_program.py
@@ -0,0 +1,13 @@
+print('Python Programming')
+#No dejar espacio al principio
+
+"""
+Multi
+Line
+Comments
+"""
+
+print("I Love \"Python\"")
+print('I Love "Python"')
+
+
---
+++ day01/variables.py
@@ -0,0 +1,44 @@
+"""
+
+Java Variable or in static type programming languages:
+    Static varNam
+
+
+Python Variables or in dynamically type programming languages:
+    varName = data
+
+"""
+
+name_of_student = "Elena"
+
+print(name_of_student)
+print(type(name_of_student))
+
+name_of_student = 3.5
+
+print(name_of_student)
+print(type(name_of_student))
+
+name_of_student = True
+
+print(name_of_student)
+print(type(name_of_student))
+
+
+s ='35'
+num = int(s)
+print(num)
+print(type(num))
+
+
+
+print("---------------------------------------------")
+
+prog_language = "Python"
+version = 3.13
+
+print("My programming language is {} programing language {}".format(prog_language, version))
+print(f"My programming language is {prog_language} programing language {version}")
+
+
+print("My programming language is " + prog_language + str(version))
